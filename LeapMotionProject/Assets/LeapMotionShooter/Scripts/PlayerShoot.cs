2a1b8fbfd1781b0b18b0e8b5c233f8ac0f15b1cc
Sacaldur <sacaldur@gmx.net>
2014-11-22 16:22:32 +0100

    Player Shooting
    Player Shooting Placeholder

---
+++ LeapMotionProject/Assets/LeapMotionShooter/Scripts/PlayerShoot.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire")) {
+			this.Shoot();
+		}
+	}
+
+
+	public void Shoot() {
+		Debug.Log("Shoting not implemented!");
+	}
+}
