1c0ef82a7e4e13f595c3308617c41b2cd365fc0e
Sacaldur <sacaldur@gmx.net>
2014-11-22 17:41:48 +0100

    Player Shoot

--- LeapMotionProject/Assets/LeapMotionShooter/Scripts/PlayerShoot.cs
+++ LeapMotionProject/Assets/LeapMotionShooter/Scripts/PlayerShoot.cs
@@ -5,6 +5,13 @@ public class PlayerShoot : MonoBehaviour {
 
 	private bool isShooting;
 
+	[SerializeField]
+	private GameObject projectilePrefab;
+
+	[SerializeField]
+	private float shootDelay;
+	private float lastShoot = -1;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -14,6 +21,13 @@ public class PlayerShoot : MonoBehaviour {
 	void Update () {
 		if(Input.GetButtonDown("Fire1")) {
 			this.StartShooting();
+		}
+
+		if(this.isShooting) {
+			if(Time.time >= this.lastShoot + this.shootDelay) {
+				this.Shoot();
+				this.lastShoot = Time.time;
+			}
 		}
 	}
 
@@ -25,5 +39,13 @@ public class PlayerShoot : MonoBehaviour {
 
 	public void StopShooting() {
 		this.isShooting = false;
+	}
+
+
+	private void Shoot() {
+		GameObject projectile = (GameObject) Object.Instantiate(this.projectilePrefab);
+
+		BulletMovement movement = projectile.GetComponent<BulletMovement>();
+		movement.Fire(Vector3.zero, new Vector3(0,0,20));
 	}
 }
