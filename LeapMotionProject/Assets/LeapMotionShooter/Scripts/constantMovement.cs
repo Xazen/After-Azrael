55cf6fdf098001ef0488587f23d722408ef8252d
Dennis Lieu <dennis.lieu@xazen.de>
2014-11-22 15:44:11 +0100

    - Move to z axis

---
+++ LeapMotionProject/Assets/LeapMotionShooter/Scripts/constantMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class constantMovement : MonoBehaviour {
+
+	[SerializeField]
+	private float movementSpeed = 5;
+
+	private Transform _transform;
+
+	// Use this for initialization
+	void Start () {
+		_transform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 movement = new Vector3 (0, 0, movementSpeed);
+
+		_transform.localPosition += movement * Time.deltaTime;
+	}
+}
