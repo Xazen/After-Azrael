e1f01e54e28253cf3558ff4ed18d6bb79d51f977
Dennis Lieu <dennis.lieu@xazen.de>
2014-11-22 15:27:51 +0100

    - Add movement scene

---
+++ LeapMotionProject/Assets/movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	public float movementSpeed = 5;
+
+	private Transform _transform;
+
+	// Use this for initialization
+	void Start () {
+		_transform = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+		_transform.position += input * Time.deltaTime * movementSpeed;
+	}
+
+
+}
