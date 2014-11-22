63d3f49c66a6636bba89657c89cba434eb551fce
Sacaldur <sacaldur@gmx.net>
2014-11-22 17:19:47 +0100

    Firebutton

--- LeapMotionProject/Assets/LeapMotionShooter/Scripts/PlayerShoot.cs
+++ LeapMotionProject/Assets/LeapMotionShooter/Scripts/PlayerShoot.cs
@@ -12,7 +12,7 @@ public class PlayerShoot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetButtonDown("Fir1e")) {
+		if(Input.GetButtonDown("Fire1")) {
 			this.StartShooting();
 		}
 	}
