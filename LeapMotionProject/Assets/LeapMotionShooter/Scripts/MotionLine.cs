d7e8fef3a72fb10af63677fee76192a70bb2f31b
Dennis Lieu <dennis.lieu@xazen.de>
2014-11-22 19:17:27 +0100

    - STUFF !

---
+++ LeapMotionProject/Assets/LeapMotionShooter/Scripts/MotionLine.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotionLine : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		Animator animation = GetComponent<Animator> ();
+		animation.speed = speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
