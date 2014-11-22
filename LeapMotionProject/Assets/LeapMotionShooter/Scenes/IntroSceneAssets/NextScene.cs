241afb5dbdc171db1904f830e661bd0f174f59e1
Sacaldur <sacaldur@gmx.net>
2014-11-22 20:58:44 +0100

    IntroScene

---
+++ LeapMotionProject/Assets/LeapMotionShooter/Scenes/IntroSceneAssets/NextScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextScene : MonoBehaviour {
+
+	[SerializeField]
+	private string name;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			Application.LoadLevel(this.name);
+		}
+	}
+}
---
+++ LeapMotionProject/Assets/LeapMotionShooter/Scenes/IntroSceneAssets/ToggleOnClick.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleOnClick : MonoBehaviour {
+	
+	[SerializeField]
+	private GameObject[] hide;
+	[SerializeField]
+	private GameObject[] show;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			foreach(GameObject obj in this.hide) {
+				obj.SetActive(false);
+			}
+			foreach(GameObject obj in this.show) {
+				obj.SetActive(true);
+			}
+		}
+	}
+}
